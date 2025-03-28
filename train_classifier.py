7a1b4ac3d4dcc8b647e1837c9013ac99a0945302
scheysej <scheysej@udmercy.edu>
2025-03-27 23:46:55 -0400

    didn't make many changes, but they are changes

--- train_classifier.py
+++ train_classifier.py
@@ -7,7 +7,21 @@ import numpy as np
 
 data_dict = pickle.load(open("data.pickle", 'rb'))
 
-data = np.array(data_dict['data'])
+print([len(item) if isinstance(item, list) else 'Not a list' for item in data_dict['data']])  # Check lengths
+
+indices_84 = [i for i, item in enumerate(data_dict['data']) if isinstance(item, list) and len(item) == 84]
+for i, item in enumerate(data_dict['data']):
+    if isinstance(item, list) and len(item) == 84:
+        data_dict['data'][i] = item[:42]  # Truncate to length 42
+
+print("Modified data where length was 84.")
+
+
+print("Indices of elements with length 84:", indices_84)
+
+
+
+data = np.array(data_dict['data'], dtype="object")
 labels = np.array(data_dict['labels'])
 
 x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
